0ff6d08e55cb451bd5c090105b57c8752a293e1e
Éderson Dantas padovani <gotikozzy@live.com>
2026-01-11 22:24:16 -0300

    ajuste js formulas matematicas

--- docs/assets/mathjax.js
+++ docs/assets/mathjax.js
@@ -1,9 +1,36 @@
 window.MathJax = {
   tex: {
-    inlineMath: [['$', '$'], ['\\(', '\\)']],
-    displayMath: [['$$', '$$'], ['\\[', '\\]']]
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
   },
   options: {
-    skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code']
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
   }
 };
+
+/* 1. Inicializa no carregamento da página (Instant Loading) */
+document$.subscribe(() => { 
+  MathJax.typesetPromise()
+})
+
+/* 2. Escuta a troca de tema e força a repintura das fórmulas */
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    if (mutation.type === "attributes") {
+      // Pequeno delay para garantir que o CSS do tema já trocou
+      setTimeout(() => {
+        MathJax.typesetPromise()
+      }, 100);
+    }
+  });
+});
+
+/* Observa mudanças no atributo 'data-md-color-scheme' no corpo do site */
+var element = document.querySelector("body");
+observer.observe(element, {
+  attributes: true,
+  attributeFilter: ["data-md-color-scheme"]
+});
